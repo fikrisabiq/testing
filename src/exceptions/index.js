8fc170cd479bfd1eed6b589ddde15835f104f2e5
fikrisabiq <81612058+fikrisabiq@users.noreply.github.com>
2026-09-17 17:45:21 +0700

    first commit

---
+++ src/exceptions/index.js
@@ -0,0 +1,11 @@
+import ClientError from './client-error.js';
+import InvariantError from './invariant-error.js';
+import NotFoundError from './not-found-error.js';
+import AuthorizationError from './authorization-error.js';
+
+export {
+  ClientError,
+  InvariantError,
+  NotFoundError,
+  AuthorizationError
+};
---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
+import 'dotenv/config';
+
+import express from 'express';
+import routes from '../routes/index.js';
+import ErrorHandler from '../middlewares/error.js';
+
+const app = express();
+app.use(ErrorHandler);
+
+app.use(express.json());
+app.use(routes);
+
+export default app;
